3433bb0c871de72f1eff2538a478c52530e6abe8
杨博 (Yang Bo) <pop.atry@gmail.com>
2017-01-15 13:15:38 +0800

    Support Windows
    This will fix issue https://github.com/krausest/js-framework-benchmark/issues/112

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var BinWrapper = require('bin-wrapper');
 var version = '0.13.9';
 
 var bin = new BinWrapper({skipCheck: true});
-bin.src('https://dl.bintray.com/sbt/native-packages/sbt/' + version + '/sbt-' + version+ '.tgz', 'darwin');
-bin.src('https://dl.bintray.com/sbt/native-packages/sbt/' + version + '/sbt-' + version+ '.tgz', 'linux');
+bin.src('https://dl.bintray.com/sbt/native-packages/sbt/' + version + '/sbt-' + version+ '.tgz');
 bin.dest(path.join(__dirname, 'vendor'));
 bin.use(process.platform === 'win32' ? 'bin/sbt.bat' : 'bin/sbt');
 
